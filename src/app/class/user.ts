75a09b26eb59f43454fd4e9deb46a0d6e870f02a
sheila <devsheilawambui@gmail.com>
2021-04-05 07:21:06 +0300

    structured user class

--- src/app/class/user.ts
+++ src/app/class/user.ts
@@ -1,2 +1,15 @@
 export class User {
+    
+  constructor(
+    public public_repos:number,
+    public followers:number,
+    public following:number,
+    public avatar_url :string,
+    public starred_url:string,
+    public login:string,
+    public html_url:string,
+    public location:string,
+    public created_at:Date){
+      
+    }
 }
