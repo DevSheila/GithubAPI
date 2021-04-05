258f499f4b2b3c8eb53f8fd9b40118d959bad2ae
sheila <devsheilawambui@gmail.com>
2021-04-05 07:49:19 +0300

    generated function to search and return user profile

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -7,8 +7,24 @@ import{ User} from '../class/user'
   styleUrls: ['./github-profile.component.css']
 })
 export class GithubProfileComponent implements OnInit {
+  userProfile!: User;
 
-  constructor() { }
+  constructor(public githubService:GithubService) { }
+
+ 
+
+  searchUser(searchName: any ){
+    console.log(searchName)
+    this.githubService.getProfile(searchName).then((response :any)=>{
+        this.userProfile = this.githubService.userProfile;
+        // this.userProfile = response;
+       
+        console.log(this.userProfile);
+      },
+      (error)=>{
+        console.log(error)
+      });
+  }
 
   ngOnInit(): void {
   }
