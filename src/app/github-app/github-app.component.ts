171e3e5d323bab97e0daff033e431bca53e5c6a4
sheila <devsheilawambui@gmail.com>
2021-04-05 07:37:14 +0300

    added function to get search input and output it

--- src/app/github-app/github-app.component.ts
+++ src/app/github-app/github-app.component.ts
@@ -8,6 +8,21 @@ import {User} from '../class/user'
 })
 export class GithubAppComponent implements OnInit {
 
+  @Output() searchOutput = new EventEmitter<any>()
+  // constructor() { }
+
+
+  userProfile = new User(0,0,0,"","","","","",new Date())
+  searchName: any ;
+
+  search(){
+    this.searchOutput.emit(this.searchName);
+    console.log(this.searchName)
+    this.userProfile = new User(0,0,0,"","","","","",new Date())
+    console.log(this.searchOutput)
+    this.searchName=""
+
+  }
   constructor() { }
 
   ngOnInit(): void {
