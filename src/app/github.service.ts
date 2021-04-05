0a3564f8e0a32c75000f94aa2afea9c96f7058fc
sheila <devsheilawambui@gmail.com>
2021-04-05 07:23:05 +0300

    imported required imports to github service

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
+import { environment} from '../environments/environment'
+import { Observable, throwError } from 'rxjs';
+import {User} from './class/user'
+import {Repositories} from './class/repositories'
 @Injectable({
   providedIn: 'root'
 })
