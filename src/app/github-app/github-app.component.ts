f1e39b43d1ce59fdc56327451dcef286b6985848
sheila <devsheilawambui@gmail.com>
2021-04-05 07:34:45 +0300

    imported required imports to search component

--- src/app/github-app/github-app.component.ts
+++ src/app/github-app/github-app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { GithubService } from '../github.service';
+import {User} from '../class/user'
 @Component({
   selector: 'app-github-app',
   templateUrl: './github-app.component.html',
