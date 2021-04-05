a8277b8d6abb7b99e619731daae50ca088ceea53
sheila <devsheilawambui@gmail.com>
2021-04-05 07:26:25 +0300

    added a function to make httpRequests and return user details

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -14,4 +14,26 @@ export class GithubService {
 
   constructor(private http :HttpClient) {
    }
+
+   public getProfile(searchQuery: String){
+
+    return new Promise((resolve, reject) => {
+      let apiURL = `https://api.github.com/users/${searchQuery}?access_token=${environment.ApiKey}`;
+      this.http.get(apiURL)
+      .toPromise()
+      .then(
+          (response:any) => {
+            this.userProfile= response; 
+            console.log(response);
+            resolve(response);
+          },
+          (error) => {
+                  console.log(error);
+                  reject();
+                }
+
+        );
+    });
+
+}
 }
