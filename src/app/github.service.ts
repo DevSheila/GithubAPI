2f992d88ac79aca83da85ce850e42ae1778b47ac
sheila <devsheilawambui@gmail.com>
2021-04-05 15:00:05 +0300

    solve environments error

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import { environment} from '../environments/environment'
+// import { environment} from '../environments/environment'
 import { Observable, throwError } from 'rxjs';
 import {User} from './class/user'
 import {Repositories} from './class/repositories'
@@ -14,13 +14,21 @@ export class GithubService {
   userRepositories = new Repositories(0,0,"","","","",new Date);
 
   constructor(private http :HttpClient) {
+    
    }
+
+  myHeaders:any= new Headers();
+  public requestOptions = {
+      method: 'GET',
+      headers: this.myHeaders.append("Authorization", "token 1234567890"),
+      redirect: 'profile'
+  };
 
   public getProfile(searchQuery: String){
 
       return new Promise((resolve, reject) => {
-        let apiURL = `https://api.github.com/users/${searchQuery}?access_token=${environment.ApiKey}`;
-        this.http.get(apiURL)
+        let apiURL = `https://api.github.com/users/${searchQuery}`;
+        this.http.get(apiURL,this.requestOptions )
         .toPromise()
         .then(
             (response:any) => {
@@ -41,8 +49,8 @@ export class GithubService {
 
   public getRepositories(searchQuery:String){
     return new Promise((resolve, reject) => {
-      let apiURL = `https://api.github.com/users/${searchQuery}/repos?access_token=${environment.ApiKey}`;
-      this.http.get(apiURL)
+      let apiURL = `https://api.github.com/users/${searchQuery}/repos?`;
+      this.http.get(apiURL,this.requestOptions)
       .toPromise()
       .then(
           (response:any) => {
