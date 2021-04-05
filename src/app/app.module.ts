516f86d9408c4ffbd048e673021551d242b0b10d
sheila <devsheilawambui@gmail.com>
2021-04-05 07:14:25 +0300

    Generated initial file structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubAppComponent } from './github-app/github-app.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubReposComponent } from './github-repos/github-repos.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DatePipe } from './date.pipe';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HighlightDirective } from './highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubAppComponent,
+    GithubProfileComponent,
+    GithubReposComponent,
+    NavbarComponent,
+    DatePipe,
+    NotFoundComponent,
+    HighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
