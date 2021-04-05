e25fc5373a9a7c34b8973f7788da7fe0eebc3948
sheila <devsheilawambui@gmail.com>
2021-04-05 07:19:02 +0300

    stuctured repositories class

--- src/app/class/repositories.ts
+++ src/app/class/repositories.ts
@@ -1,2 +1,12 @@
 export class Repositories {
+    constructor(
+        public forks:number,
+        public watchers_count:number,
+        public name:string,
+        public html_url:string,
+        public description:string,
+        public language:string,
+        public created_at:Date){
+          
+        }
 }
