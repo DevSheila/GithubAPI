7fa86d73a5156a78e7214725dbdffb6fc68f05f4
sheila <devsheilawambui@gmail.com>
2021-04-06 07:46:46 +0300

    added hightlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener,  } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { 
+  }
+ 
+  @HostListener("mouseenter")onMouseEnter(){
+    this.textDeco("black")
+  }
+
+  @HostListener("mouseleave")onMouseLeave(){
+    this.textDeco("")
+  }
+ 
+  private textDeco(action:string){
+    this.elem.nativeElement.style.background=action;
+  }
+  // parentNode.parentNode.parentNode
+ 
 
 }
